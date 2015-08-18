794864eb2e39bc651e80aee73ad906c8f441e29e
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-08-18 12:54:54 -0400

    use correct git endpoint

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ Dotenv.load
 
 
 set :application, "rocky"
-set :repository,  "git@github.com:trustthevote/Rocky.git"
+set :repository,  "git@github.com:trustthevote-project/Rocky-OVR.git"
 
 # If you have previously been relying upon the code to start, stop
 # and restart your mongrel application, or if you rely on the database
@@ -172,7 +172,7 @@ namespace :deploy do
   end
   
   desc "import states.yml and zip code data"
-  task :import_states_and_zips, :roles=>[:app] do
+  task :import_states_and_zips, :roles=>[:util] do
     run <<-CMD
       cd #{latest_release} &&
       bundle exec rake import:states_and_zips
