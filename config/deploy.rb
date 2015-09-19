edac33e7a6189e250ee1361e1297357f9cffae5f
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-09-18 22:15:13 -0400

    sizing for loadtest instances

--- config/deploy.rb
+++ config/deploy.rb
@@ -312,7 +312,7 @@ namespace :deploy do
   task :run_util_pdf_workers, :roles => :util do
     run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec ruby script/rocky_pdf_runner stop"
     sleep 10
-    2.times do
+    8.times do
       run "cd #{latest_release} && RAILS_ENV=#{rails_env} TZ=:/etc/localtime bundle exec ruby script/rocky_pdf_runner start"
     end
   end
