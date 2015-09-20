43021a4ce9e19065c1dd27ebebe2e06eec3b475a
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-09-20 12:11:10 -0400

    more pdfgen logging

--- lib/pdf_writer.rb
+++ lib/pdf_writer.rb
@@ -208,17 +208,21 @@ class PdfWriter
 
 
   def self.write_pdf_from_html_string(html_string, path, locale, pdf_file_dir)
+    t = Time.now
     pdf = WickedPdf.new.pdf_from_string(
       html_string,
       :disable_internal_links         => false,
       :disable_external_links         => false,
       :encoding => 'utf8',
       :locale=>locale
-    )      
+    )
+    Rails.logger.info("#{Time.now - t} to generate pdf")
     FileUtils.mkdir_p(pdf_file_dir)
+    Rails.logger.info("#{Time.now - t} to make pdf dir")
     File.open(path, "w") do |f|
       f << pdf.force_encoding('UTF-8')
     end
+    Rails.logger.info("#{Time.now - t} to write pdf to disk")
   end   
   
   private 
