227bcde63c0b7508f5d3cab65a97faa1bd0e284a
Alex Mekelburg <alex.mekelburg@gmail.com>
2015-10-08 11:49:59 -0400

    change migration

--- db/migrate/20151008153652_add_index_to_registrants_finish_with_state.rb
+++ db/migrate/20151008153652_add_index_to_registrants_finish_with_state.rb
@@ -1,5 +1,5 @@
 class AddIndexToRegistrantsFinishWithState < ActiveRecord::Migration
   def change
-    add_index :registrants, :finish_with_state
+    add_index :registrants, [:finish_with_state, :partner_id, :status, :created_at], name: :index_registrants_for_stats
   end
 end
