1aa4f8fa0b7f4e382a6cd6bffa61cefd49beb29c
Alex Mekelburg <alex@mekware.com>
2016-06-02 09:56:59 -0400

    dont install dev/test gems

--- script/aws/rails_setup.sh
+++ script/aws/rails_setup.sh
@@ -3,7 +3,7 @@ cd /var/www/rocky
 source /home/ec2-user/.rvm/scripts/rvm 
 rvm use ruby-1.9.3-p551@rocky6
 # Make sure we have the config files downloaded
-bundle install
+bundle install --without development test
 # bundle exec rake db:migrate
 # bundle exec rake import:states
 
