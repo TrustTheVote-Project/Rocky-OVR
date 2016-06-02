3f475787b7497ae9d5c2f21d657e548d47f5a522
Alex Mekelburg <alex@mekware.com>
2016-06-02 13:40:23 -0400

    fix conf location, add bootstrap

--- script/aws/rails_setup.sh
+++ script/aws/rails_setup.sh
@@ -10,6 +10,8 @@ aws s3 cp s3://rocky-staging2-codedeploy/database.yml config/database.yml --regi
 
 # TODO: get these to use a standard environment variable?
 RAILS_ENV=staging2 bundle exec rake db:migrate
+# TODO: Only run once!!
+RAILS_ENV=staging2 bundle exec rake db:bootstrap
 RAILS_ENV=staging2 bundle exec rake import:states
 
 # touch tmp/restart.txt
