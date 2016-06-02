23ea97778f22d3f2cdc6fe221bc8e4e631dc03dd
Alex Mekelburg <alex@mekware.com>
2016-06-02 09:51:44 -0400

    conditionally install passenger, make sure git is installed

--- script/aws/install_rvm_passenger.sh
+++ script/aws/install_rvm_passenger.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# TODO: test if this is all set-up, if so don't run these!
 cd /home/ec2-user
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-# Most of the time gpg2 is not necessary, so keep going if it doesn't work
-# If it's a new setup and doesn't work, the rvm install will fail anyway
-curl -sSL https://get.rvm.io | /bin/bash -s stable && \
-    source /home/ec2-user/.rvm/scripts/rvm && \
-    rvm install 1.9.3-p551 && \
-    rvm use ruby-1.9.3-p551@rocky6 --create && \
-    gem install passenger --version=3.0.19 && \
-    passenger-install-apache2-module --auto && \
-    rvm use ruby-1.9.3-p551@rocky6 && gem install bundler
+PASSENGER_VERSION=$(passenger-config --version)
+
+if [ $PASSENGER_VERSION != '3.0.19' ]; then
+    gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+    # Most of the time gpg2 is not necessary, so keep going if it doesn't work
+    # If it's a new setup and doesn't work, the rvm install will fail anyway
+    curl -sSL https://get.rvm.io | /bin/bash -s stable && \
+        source /home/ec2-user/.rvm/scripts/rvm && \
+        rvm install 1.9.3-p551 && \
+        rvm use ruby-1.9.3-p551@rocky6 --create && \
+        gem install passenger --version=3.0.19 && \
+        passenger-install-apache2-module --auto && \
+        rvm use ruby-1.9.3-p551@rocky6 && gem install bundler
+fi
