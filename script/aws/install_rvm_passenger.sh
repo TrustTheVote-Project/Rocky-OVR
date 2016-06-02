30d900a7a37fd6f37602e5f9285fd6b22fdd5c65
Alex Mekelburg <alex@mekware.com>
2016-06-02 08:40:21 -0400

    run as root

--- script/aws/install_rvm_passenger.sh
+++ script/aws/install_rvm_passenger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | /bin/bash -s stable --ruby=1.9.3-p125
+curl -sSL https://get.rvm.io | /bin/bash -s stable --ruby=1.9.3-p125  --add-to-rvm-group ec2-user
 rvm use 1.9.3-p125@rocky6
 gem install passenger --version=3.0.19
 passenger-install-apache2-module --auto
