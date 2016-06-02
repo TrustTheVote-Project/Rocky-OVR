7a354beed4d6363c36f17ee4352b7bc4b9aa80be
Alex Mekelburg <alex@mekware.com>
2016-06-02 16:01:55 -0400

    move server files to s3

--- script/aws/install_lib_dependencies.sh
+++ script/aws/install_lib_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+aws s3 cp s3://rocky-staging2-codedeploy/RPM-GPG-KEY-CentOS-6 /etc/pki/rpm-pkg/ --region us-west-2
+aws s3 cp s3://rocky-staging2-codedeploy/centos-base.repo /etc/yum.repos.d/ --region us-west-2
+
 yum install -y git-all httpd apr-devel apr-util-devel gcc-c++ httpd-devel ImageMagick-devel libcurl-devel libxslt-devel libyaml-devel mysql-devel perl-DBD-MySQL perl-DBI perl-XML-Simple zlib-devel openssl-devel mod_ssl
 
 
@@ -9,3 +12,6 @@ yum install -y baekmuk-ttf-batang-fonts baekmuk-ttf-dotum-fonts baekmuk-ttf-font
 yum install -y  khmeros-base-fonts khmeros-battambang-fonts khmeros-bokor-fonts khmeros-fonts-common khmeros-handwritten-fonts khmeros-metal-chrieng-fonts khmeros-muol-fonts khmeros-siemreap-fonts
 
 yum install -y  lohit-bengali-fonts lohit-devanagari-fonts samyak-devanagari-fonts samyak-fonts-common
+
+
+aws s3 cp s3://rocky-staging2-codedeploy/httpd-staging2.conf /etc/httpd/conf/httpd.conf --region us-west-2
--- script/aws/rails_setup.sh
+++ script/aws/rails_setup.sh
@@ -7,6 +7,7 @@ bundle install --without development test
 
 # Make sure we have the config files downloaded
 aws s3 cp s3://rocky-staging2-codedeploy/database.yml config/database.yml --region us-west-2
+aws s3 cp s3://rocky-staging2-codedeploy/.env.staging2 .env.staging2 --region us-west-2
 
 # TODO: get these to use a standard environment variable?
 RAILS_ENV=staging2 bundle exec rake db:migrate
