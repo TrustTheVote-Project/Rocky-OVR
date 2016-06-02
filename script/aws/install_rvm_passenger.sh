fe16a290a0f1f5e84d9bb37f3c23a4a559b9a7ad
Alex Mekelburg <alex@mekware.com>
2016-06-02 08:50:57 -0400

    ec2-user should be fine, make sure ruby installs and source the rvm command

--- script/aws/install_rvm_passenger.sh
+++ script/aws/install_rvm_passenger.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd /home/ec2-user
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | /bin/bash -s stable --ruby=1.9.3-p125  --add-to-rvm-group ec2-user
-rvm use 1.9.3-p125@rocky6
-gem install passenger --version=3.0.19
-passenger-install-apache2-module --auto
-rvm use ruby-1.9.3-p125@rocky6 && gem install bundler
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
+    curl -sSL https://get.rvm.io | /bin/bash -s stable && \
+    source /home/ec2-user/.rvm/scripts/rvm && \
+    rvm install 1.9.3-p125 && \
+    rvm use 1.9.3-p125@rocky6 && \
+    gem install passenger --version=3.0.19 && \
+    passenger-install-apache2-module --auto && \
+    rvm use ruby-1.9.3-p125@rocky6 && gem install bundler
