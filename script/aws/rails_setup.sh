62384f16cdac8697bea68c8987a87b0eb7f25e30
Alex Mekelburg <alex@mekware.com>
2016-06-02 13:08:25 -0400

    conditions for install, get database.yml from s3

--- script/aws/rails_setup.sh
+++ script/aws/rails_setup.sh
@@ -2,8 +2,12 @@
 cd /var/www/rocky
 source /home/ec2-user/.rvm/scripts/rvm 
 rvm use ruby-1.9.3-p551@rocky6
-# Make sure we have the config files downloaded
+gem install bundler
 bundle install --without development test
+
+# Make sure we have the config files downloaded
+aws s3 cp s3://rocky-staging2-codedeploy/database.yml config/database.yml --region us-west-2
+
 # bundle exec rake db:migrate
 # bundle exec rake import:states
 
