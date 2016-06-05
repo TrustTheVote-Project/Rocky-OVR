f791201b03a687522257e322db6a9837a9deee63
Alex Mekelburg <alex@mekware.com>
2016-06-05 03:30:32 -0400

    fix beforescript

--- script/aws/backup_previous_revision.sh
+++ script/aws/backup_previous_revision.sh
@@ -1,4 +1,6 @@
 # TODO Before downloading the latest code, back up existing code base
 # Have the app served from a symlinked location, so we can just change the pointer for a quick revert
-mkdir -p /var/www/rocky-last-deploy
+sudo mkdir -p /var/www/rocky-last-deploy
+sudo chown ec2-user /var/www/rocky-last-deploy
+sudo chgrp ec2-user /var/www/rocky-last-deploy
 cp -r /var/www/rocky/* /var/www/rocky-last-deploy/
