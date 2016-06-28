cb8e6c043923de8e2eee135e1824fe008e820d11
Alex Mekelburg <alex@mekware.com>
2016-06-28 16:10:33 -0400

    dont set proxy

--- lib/integrations/soap.rb
+++ lib/integrations/soap.rb
@@ -5,7 +5,7 @@ module Integrations
       if Rails.env.development?
         return File.new(Rails.root.join("spec/fixtures/files/covr/max_registrant_response.xml")).read
       else
-        set_proximo_proxy
+        #set_proximo_proxy
         return RestClient::Request.execute(:method=>:post, :payload=>xml_content, :url=>url, :headers=>{:content_type=>"application/soap+xml;charset=UTF-8"}, :timeout=>17, :open_timeout=>3)
         #return RestClient.post(url, xml_content, :content_type=>"application/soap+xml;charset=UTF-8", :timeout => 15)
       end
