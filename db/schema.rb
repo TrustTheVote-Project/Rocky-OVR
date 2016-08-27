48a417e31d9200489cd33f3cd8956e9069431dd2
Alex Mekelburg <alex@mekware.com>
2016-08-27 13:33:19 -0400

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20160827171640) do
+
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
     t.integer  "attempts",   :default => 0
