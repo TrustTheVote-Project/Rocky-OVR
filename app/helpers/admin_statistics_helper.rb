a748b2060431b5338360f60fc31f6a4e213e36ea
Vlad Chervanev <vlad.chervanev@castle.co>
2016-09-13 22:44:14 +0400

    Admin stats: formatting

--- app/helpers/admin_statistics_helper.rb
+++ app/helpers/admin_statistics_helper.rb
@@ -10,12 +10,14 @@ module AdminStatisticsHelper
     downloads_count = Registrant.where(conditions, false, true).group("partner_id").count
     not_down_count = Registrant.where(conditions, false, false).group("partner_id").count
     ids = (downloads_count.keys + not_down_count.keys).uniq.sort
+    # if we decide to load all partners regardless empty registrants list
+    # ids = names.keys.sort
 
     ids.map do |id|
       c1 = downloads_count[id] || 0
       c2 = not_down_count[id] || 0
       total = c1 + c2
-      percent = total != 0 ? (100*c1.to_f/total).round(2) : "NA"
+      percent = total != 0 ? "#{(100*c1.to_f/total).round(2)}%" : "N/A"
       [id, names[id], percent]
     end
   end
