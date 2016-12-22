0522cddda592452b87fc9ff2d2c386c6fb30481c
Alex Mekelburg <alex@mekware.com>
2016-12-22 07:43:35 -0500

    link to correct embed code, alwys show mobile regardless of whitelabel partner

--- app/controllers/registration_step.rb
+++ app/controllers/registration_step.rb
@@ -131,7 +131,7 @@ class RegistrationStep < ApplicationController
   def set_params
     @source = params[:source]
     @tracking = params[:tracking]
-    @short_form = params[:short_form]!="false" && (!@partner || !@partner.use_long_form?)
+    @short_form = (params[:short_form]!="false" && (!@partner || !@partner.use_long_form?)) || MobileConfig.is_mobile_request?(request)
     @collect_email_address = params[:collectemailaddress]
     @email_address = params[:email_address]
     @first_name = params[:first_name]
