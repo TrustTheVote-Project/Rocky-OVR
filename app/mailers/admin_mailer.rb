547e4c135e4be4d3e8ac53167d00afd79544f177
Alex Mekelburg <alex@mekware.com>
2018-03-08 12:55:51 -0500

    include environment name in admin emails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -55,7 +55,7 @@ class AdminMailer < ActionMailer::Base
   
   def pa_no_registrant_error(registrant_id)
     mail(
-      subject: "[ROCKY PA INTEGRATION] Error submitting to PA: No registrant found for id #{registrant_id}",
+      subject: "[ROCKY PA INTEGRATION#{environment_subject}] Error submitting to PA: No registrant found for id #{registrant_id}",
       body: "Registrant #{registrant_id} not found in the rocky database to submit to PA"
     )
   end
@@ -64,7 +64,7 @@ class AdminMailer < ActionMailer::Base
     name = registrant ? "#{registrant.first_name} #{registrant.last_name}" : "(name not determined)"
     registrant_details = registrant ? "\nEvent Name: #{registrant.open_tracking_id}\nEvent Zip: #{registrant.tracking_id}\nCanvasser Namer: #{registrant.tracking_source}" : nil
     mail(
-      subject:"[ROCKY GROMMET] Error validating request from grommet",
+      subject:"[ROCKY GROMMET#{environment_subject}] Error validating request from grommet",
       body: "Registrant - #{name} - not registered due to validation error:#{registrant_details}\n\n#{error_list.join('\n')}"
     )
   end
@@ -72,9 +72,16 @@ class AdminMailer < ActionMailer::Base
   def pa_registration_error(registrant, error_list)
     
     mail(
-      subject: "[ROCKY PA INTEGRATION] Error submitting registration #{registrant.class} #{registrant.id} to PA",
+      subject: "[ROCKY PA INTEGRATION#{environment_subject}] Error submitting registration #{registrant.class} #{registrant.id} to PA",
       body: "PA system returned the error:\n\n #{error_list.join("\n")}"
     )
+  end
+  
+  private
+  
+  def environment_subject
+    return '' if Rails.env.production?
+    return " - #{Rails.env}"
   end
   
 end
