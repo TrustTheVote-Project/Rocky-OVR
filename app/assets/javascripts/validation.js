d0fbc221064f571a9f58e337364ca9e1bb1c42a1
Alex Mekelburg <alex@mekware.com>
2019-02-15 10:26:54 -0500

    run validations on all previous fields

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -20,7 +20,7 @@ function validateField(errorMessage) {
       currentError.splice(messageIdx, errorMessage.length)
       errorField.text(currentError.join(''))
     } 
-    console.log(errorField.text())
+    //console.log(errorField.text())
     if (errorField.text().replace(/\s/g, '') == '') {
       parent.removeClass('has_error') 
     }
@@ -67,49 +67,31 @@ function initValidations() {
     $(this).change(validateField.bind(this, errorMessage))
     $(this).keyup(validateField.bind(this, errorMessage))
   })
-  initCustomValidations();
-}
-
-function initCustomValidations() {
-  // When name fields have content and title is blank
-  $("input").blur(function() {
-    var firstName = $("#registrant_first_name, #state_registrants_va_registrant_first_name, #state_registrants_pa_registrant_first_name").val()
-    var lastName = $("#registrant_last_name, #state_registrants_va_registrant_last_name, #state_registrants_pa_registrant_last_name").val()
-    if (firstName != '' && lastName != '') {
-      var titleField = $("#registrant_name_title, #state_registrants_va_registrant_name_title, #state_registrants_pa_registrant_name_title");
-      var errorMessage = $(titleField).data("client-validation-required")
-      validateField.bind(titleField)(errorMessage)
+  
+  $("[data-client-validation-require-accept]").each(function() {
+    var errorMessage = $(this).data("client-validation-require-accept");
+    $(this).change(validateBooleanField.bind(this,errorMessage))
+  })
+  
+  var fields = $("input, select, textarea");
+  var validatePrevious = function(element) {
+    for (var i=0,ii=fields.length;i<ii;i++) {
+      var field = fields[i]
+      if (field == element) {
+        break;
+      } else {
+        var errorMessage = $(field).data("client-validation-required")  
+        if (errorMessage && !errorMessage == '') {
+          validateField.bind(field)(errorMessage);
+        }      
+        errorMessage = $(field).data("client-validation-require-accept")
+        if (errorMessage && !errorMessage == '') {
+          validateBooleanField.bind(field)(errorMessage)    
+        }      
+      }
     }
-  })
-  $("#registrant_home_address, #registrant_us_citizen, #state_registrants_va_registrant_registration_address_1, #state_registrants_va_registrant_confirm_us_citizen, #state_registrants_va_registrant_date_of_birth, #state_registrants_pa_registrant_confirm_us_citizen, #state_registrants_pa_registrant_registration_address_1").click(function() {
-    var field = $('#registrant_us_citizen, #state_registrants_va_registrant_confirm_us_citizen, #state_registrants_pa_registrant_confirm_us_citizen');
-    var errorMessage = $(field).data("client-validation-require-accept")
-    validateBooleanField.bind(field)(errorMessage)    
-  })
-  $("#registrant_home_address, #state_registrants_va_registrant_registration_address_1, #state_registrants_va_registrant_date_of_birth, #state_registrants_pa_registrant_registration_address_1").focus(function() {
-    var field = $('#registrant_us_citizen, #state_registrants_va_registrant_confirm_us_citizen, #state_registrants_pa_registrant_confirm_us_citizen');
-    var errorMessage = $(field).data("client-validation-require-accept")
-    validateBooleanField.bind(field)(errorMessage)    
-  })
-
-  $("#registrant_home_address, #registrant_us_citizen, #state_registrants_va_registrant_registration_address_1, #state_registrants_va_registrant_confirm_us_citizen, #state_registrants_va_registrant_date_of_birth, #state_registrants_va_registrant_confirm_voter_record_update, #state_registrants_pa_registrant_registration_address_1, #state_registrants_pa_registrant_confirm_will_be_18").click(function() {
-    var field = $('#state_registrants_va_registrant_confirm_voter_record_update, #state_registrants_pa_registrant_confirm_will_be_18');
-    var errorMessage = $(field).data("client-validation-require-accept")
-    validateBooleanField.bind(field)(errorMessage)    
-  })
-  
-  $("#registrant_home_address, #state_registrants_va_registrant_registration_address_1, #state_registrants_va_registrant_date_of_birth, #state_registrants_pa_registrant_registration_address_1").focus(function() {
-    var field = $('#state_registrants_va_registrant_confirm_voter_record_update, #state_registrants_pa_registrant_confirm_will_be_18');
-    var errorMessage = $(field).data("client-validation-require-accept")
-    validateBooleanField.bind(field)(errorMessage)    
-  })
-  
-  
-  
-  // $("#registrant_home_address, #registrant_will_be_18_by_election").click(function() {
-  //   var citizenField = $('#registrant_will_be_18_by_election').get(0);
-  //   var errorMessage = $("#registrant_will_be_18_by_election").data("client-validation-require-accept")
-  //   validateBooleanField.bind(citizenField)(errorMessage)
-  // })
-  
+  }
+  fields.click(function() { validatePrevious(this) });
+  fields.focus(function() { validatePrevious(this) });  
 }
+
