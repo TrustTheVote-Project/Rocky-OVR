ee3fbd6613062f8bb2c9c3abb383eced30e16a58
Alex Mekelburg <alex@mekware.com>
2019-05-14 14:15:58 -0400

    handle messages

--- app/controllers/ses_controller.rb
+++ app/controllers/ses_controller.rb
@@ -5,12 +5,16 @@ class SesController < ApplicationController
     json = params
     begin     
       json = JSON.parse(request.raw_post)
+      json2 = JSON.load(json['Message'])
     rescue Exception => e
       raise e
     end
     SesNotification.create(request_params: json)
     #logger.info "bounce callback from AWS with #{json}"
     aws_needs_url_confirmed = json['SubscribeURL']
+    type = json2['notificationType']
+
+    
     if aws_needs_url_confirmed
       logger.info "AWS is requesting confirmation of the bounce handler URL"
       uri = URI.parse(aws_needs_url_confirmed)
@@ -18,10 +22,10 @@ class SesController < ApplicationController
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.get(uri.request_uri)
-    else
+    elsif json2 && json2['notificationType']=='Bounce'
       # Process bounces
-      if json['bounce'] && json['bounce']['bouncedRecipients']
-        json['bounce']['bouncedRecipients'].each do |recipient|
+      if json2['bounce'] && json2['bounce']['bouncedRecipients']
+        json2['bounce']['bouncedRecipients'].each do |recipient|
           #logger.info "AWS SES received a bounce on an email send attempt to #{recipient['emailAddress']}"
           e = EmailAddress.find_or_create_by(email_address: recipient['emailAddress'].to_s.strip)
           e.blacklisted = true
