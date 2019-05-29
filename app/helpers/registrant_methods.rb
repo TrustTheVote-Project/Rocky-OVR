c92d5182487fdb8a5726f205f95ee3e8e36e0c13
Alex Mekelburg <alex@mekware.com>
2019-05-29 12:52:58 -0400

    fix DOB validations

--- app/helpers/registrant_methods.rb
+++ app/helpers/registrant_methods.rb
@@ -76,14 +76,23 @@ module RegistrantMethods
     write_attribute(:date_of_birth, dob)
   end
 
+  def date_of_birth_day
+    date_of_birth&.day || @date_of_birth_day
+  end
   def date_of_birth_day=(string_value)
     @date_of_birth_day= string_value
     set_date_of_birth_from_parts
+  end
+  def date_of_birth_month
+    date_of_birth&.month || @date_of_birth_month
   end
   def date_of_birth_month=(string_value)
     @date_of_birth_month= string_value
     set_date_of_birth_from_parts
   end
+  def date_of_birth_year
+    date_of_birth&.year || @date_of_birth_year
+  end    
   def date_of_birth_year=(string_value)
     @date_of_birth_year= string_value
     set_date_of_birth_from_parts
@@ -102,7 +111,10 @@ module RegistrantMethods
       dmy_string = date_of_birth_from_parts 
       if matches = dmy_string.to_s.match(/\A(\d{1,2})\D+(\d{1,2})\D+(\d{1,4})\z/)
         m,d,y = matches.captures
-        self.date_of_birth = Date.civil(y.to_i, m.to_i, d.to_i) 
+        begin
+          self.date_of_birth = Date.civil(y.to_i, m.to_i, d.to_i) 
+        rescue
+        end
       end
     else
       self.date_of_birth = nil
@@ -131,7 +143,11 @@ module RegistrantMethods
       return
     end
     if date_of_birth_before_type_cast.blank?
-      errors.add(:date_of_birth, :blank)
+      if date_of_birth_parts.compact.length == 3
+        errors.add(:date_of_birth, :invalid)
+      else
+        errors.add(:date_of_birth, :blank)
+      end
     else
       @raw_date_of_birth = date_of_birth_before_type_cast
       date = nil
