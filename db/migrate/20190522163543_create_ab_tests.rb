0d7e65728f92cc618f5d730f7097a6916c4eff9d
Alex Mekelburg <alex@mekware.com>
2019-09-06 17:33:34 -0400

    fix migration?

--- db/migrate/20190522163543_create_ab_tests.rb
+++ db/migrate/20190522163543_create_ab_tests.rb
@@ -1,10 +1,12 @@
 class CreateAbTests < ActiveRecord::Migration
   def change
-    create_table :ab_tests do |t|
-      t.integer :registrant_id
-      t.string :name
-      t.string :assignment
-      t.timestamps null: false
+    unless ActiveRecord::Base.connection.table_exists? 'ab_tests'
+      create_table :ab_tests do |t|
+        t.integer :registrant_id
+        t.string :name
+        t.string :assignment
+        t.timestamps null: false
+      end
     end
     add_index :ab_tests, :registrant_id
     add_index :ab_tests, :name
