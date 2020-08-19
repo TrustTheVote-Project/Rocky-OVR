1c8b47e40bc5042c09d69bbf0efc803e587a7303
Christopher <cwisdo@gnaritas.com>
2020-08-19 15:53:22 -0400

    Adds interface for Arizona overloaded ID

--- app/helpers/abr_state_methods/az.rb
+++ app/helpers/abr_state_methods/az.rb
@@ -56,12 +56,12 @@ module AbrStateMethods::AZ
     "Email_Address": {
       method: "email"
     },
-    "Place_of_Birth_or_Drivers_licence_or_last_4_ssn": {sensitive: true},
+    "Place_of_Birth_or_Drivers_licence_or_last_4_ssn": {sensitive: true, method: "identification_data"},
     #Date
     #voter_signature
 
   }
-  EXTRA_FIELDS = ["has_mailing_address"]
+  EXTRA_FIELDS = ["has_mailing_address", "identification_selection",  "drivers_license_state", "place_of_birth", "last_4_ssn"]
   # e.g.
   # EXTRA_FIELDS = ["has_mailing_address", "identification"]
   
@@ -86,7 +86,12 @@ module AbrStateMethods::AZ
         "Yuma",
       ]}},
       # TODO  Make POB/dln/ssn4 separate with checkboxes to toggle fields on/off.
-      {"Place_of_Birth_or_Drivers_licence_or_last_4_ssn": {required: true, regexp:/\A{[[:alpha:]]\d{8}|\d{9}\z/}},
+      {"identification_selection": {required:true, type: :radio, options: ["place_of_birth", "drivers_license_state","last_4_ssn"]}},
+      {"drivers_license_state": {required: :is_visible, regexp:/\A{[[:alpha:]]\d{8}|\d{9}\z/, min:8, max:9, visible: "identification_selection_drivers_license_state"}},
+      {"last_4_ssn": {required: :is_visible,  min:4, max:4, visible:"identification_selection_last_4_ssn"}},
+      {"place_of_birth": {required: :is_visible, min:1, visible: "identification_selection_place_of_birth"}},
+
+      #{"Place_of_Birth_or_Drivers_licence_or_last_4_ssn": {required: true, regexp:/\A{[[:alpha:]]\d{8}|\d{9}\z/}},
       {"has_mailing_address": {type: :checkbox}},
       {"Mailing_Address": {visible: "has_mailing_address"}},
     ]
@@ -111,11 +116,37 @@ module AbrStateMethods::AZ
       self.send("#{self.class.make_method_name("Every Election I authorize the County Recorder to include my name on the PEVL and automatically send")}=", "On")
     end
   end
+
+  def identification_data
+    case self.identification_selection
+    when "drivers_license_state"
+     return (self.drivers_license_state())
+    when "last_4_ssn"
+      return (self.last_4_ssn())
+    when "place_of_birth"
+      return (self.place_of_birth())
+    else
+      return "Missing identification"
+    end
+  end
+
+
+
+
+
+
+
+    
   
   def custom_form_field_validations
     if self.has_mailing_address.to_s == "1"
       custom_validates_presence_of("Mailing_Address")
     end
+
+    if self.identification_selection
+      custom_validates_presence_of(self.identification_selection)
+    end
+
   end    
   
  
