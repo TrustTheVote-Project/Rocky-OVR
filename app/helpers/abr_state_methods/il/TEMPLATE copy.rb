ac801dd57163b3bde60459268bf85ff20581f344
Alexander Mekelburg <alexmek@gmail.com>
2020-08-24 12:28:41 -0400

    fix template copy

--- app/helpers/abr_state_methods/il/TEMPLATE copy.rb
+++ app/helpers/abr_state_methods/il/TEMPLATE copy.rb
@@ -1,47 +1,47 @@
-module AbrStateMethods::IL::
-  
-  PDF_FIELDS = {
-    
-  }
-  EXTRA_FIELDS = []
-  # e.g.
-  # EXTRA_FIELDS = ["has_mailing_address", "identification"]
-  
-  # def whatever_it_is_you_came_up_with
-  #   # TODO when blah is selected it should be "abc" and otherwise left blank
-  # end
-  
-  
-  def form_field_items
-    [
-      {"County": {type: :select, required: true, include_blank: true, options: [
-
-      ]}},
-      {"has_mailing_address": {type: :checkbox}},
-    ]
-  end
-  #e.g.
-  # [
-  #   {"reason_instructions": {type: :instructions}}, *"reason_instructions" does NOT get put into EXTRA_FIELDS
-  #   {"County": {type: :select, required: true, include_blank: true, options: [
-  #     "Adams",
-  #   ]}},
-  #   {"Security Number": {required: true}},
-  #   {"State": {visible: "has_mailing_address", type: :select, options: GeoState.collection_for_select, include_blank: true, }},
-  #   {"ZIP_2": {visible: "has_mailing_address", min: 5, max: 10}},
-  #   {"identification": {
-  #     type: :radio,
-  #     required: true,
-  #     options: ["dln", "ssn4", "photoid"]}},
-  #   {"OR": {visible: "identification_dln", min: 8, max: 8, regexp: /\A[a-zA-Z]{2}\d{6}\z/}},
-  #   {"OR_2": {visible: "identification_ssn4", min: 4, max: 4, regexp: /\A\d{4}\z/}},
-  # ]
-  
-  
-  def custom_form_field_validations
-    # make sure delivery is selected if reason ==3
-    # make sure fax is provided if faxtype is selected for delivery
-  end
-  
- 
-end
+# module AbrStateMethods::IL::
+#
+#   PDF_FIELDS = {
+#
+#   }
+#   EXTRA_FIELDS = []
+#   # e.g.
+#   # EXTRA_FIELDS = ["has_mailing_address", "identification"]
+#
+#   # def whatever_it_is_you_came_up_with
+#   #   # TODO when blah is selected it should be "abc" and otherwise left blank
+#   # end
+#
+#
+#   def form_field_items
+#     [
+#       {"County": {type: :select, required: true, include_blank: true, options: [
+#
+#       ]}},
+#       {"has_mailing_address": {type: :checkbox}},
+#     ]
+#   end
+#   #e.g.
+#   # [
+#   #   {"reason_instructions": {type: :instructions}}, *"reason_instructions" does NOT get put into EXTRA_FIELDS
+#   #   {"County": {type: :select, required: true, include_blank: true, options: [
+#   #     "Adams",
+#   #   ]}},
+#   #   {"Security Number": {required: true}},
+#   #   {"State": {visible: "has_mailing_address", type: :select, options: GeoState.collection_for_select, include_blank: true, }},
+#   #   {"ZIP_2": {visible: "has_mailing_address", min: 5, max: 10}},
+#   #   {"identification": {
+#   #     type: :radio,
+#   #     required: true,
+#   #     options: ["dln", "ssn4", "photoid"]}},
+#   #   {"OR": {visible: "identification_dln", min: 8, max: 8, regexp: /\A[a-zA-Z]{2}\d{6}\z/}},
+#   #   {"OR_2": {visible: "identification_ssn4", min: 4, max: 4, regexp: /\A\d{4}\z/}},
+#   # ]
+#
+#
+#   def custom_form_field_validations
+#     # make sure delivery is selected if reason ==3
+#     # make sure fax is provided if faxtype is selected for delivery
+#   end
+#
+#
+# end
