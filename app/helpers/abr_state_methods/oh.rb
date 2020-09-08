ff40e9a3c00e89ac93ace80d279a6fc358791dbc
Christopher <cwisdo@gnaritas.com>
2020-09-08 17:04:02 -0400

    Updates Ohio PDF

--- app/helpers/abr_state_methods/oh.rb
+++ app/helpers/abr_state_methods/oh.rb
@@ -13,42 +13,63 @@ module AbrStateMethods::OH
     "Suffix": {
       method: "name_suffix"
     },
-    "Date of Birth do not write todays date here": {
-      method: "date_of_birth_mm_dd_yyyy"
+    # Old "Date of Birth do not write todays date here": {
+    #  method: "date_of_birth_mm_dd_yyyy"
+    #},
+    "DOB_day": {
+      method:"date_of_birth_mm"  # Note this is reversed in form
     },
-    "Street Address no PO  boxes": {
+    "DOB_mo": {
+      method: "date_of_birth_dd" # Note this is reversed in form
+    },
+    "DOB_yr": {
+      method: "date_of_birth_yyyy"
+    },
+    #"Street Address no PO  boxes": {
+    "Street Address": {
       method: "address"
     },
     "County": {},
-    "CityVillage": {
-      method: "city"
-    },
+    # Old "CityVillage": {
+    #  method: "city"
+    #},
+    "City": {},
     "ZIP": {
       method: "zip"
     },
     #mailing address
-    "Street Address or PO  box": {},
-    "CityVillage_2": {},
+    #old "Street Address or PO  box": {},
+    "Street Address or PO Box": {},
+    # old "CityVillage_2": {},
+    "City_2": {},
     "State": {},
     "ZIP_2": {},
-    "OR": {sensitive: true}, #drivers license number
-    "Your Ohio driver license number 2 letters followed by 6 numbers": {
+    #"OR": {sensitive: true}, #drivers license number
+    "Ohio_driver_license": {sensitive: true},
+    
+=begin  
+     "Your Ohio driver license number 2 letters followed by 6 numbers": {
       options: ["Off", "On"]
     },
     "Last four digits of your Social Security number": {
+   
       options: ["Off", "On"]
     },
     "Copy of a current and valid photo identification military identification or a current within the": {
       options: ["Off", "On"]
     },
-    "OR_2": {sensitive: true}, #last 4 of SS
+=end
+    #"OR_2": {sensitive: true}, #last 4 of SS
+    "SSN4": {sensitive: true},
     "Date of Election do not write todays date here": {
       value: "11/03/2020"
     },
-    "General Election": {
+    #old "General Election": {
+    "November General Election": {
       options: ["Off", "On"],
       value: "On"
     },
+    
     "Primary Election": {
       options: ["Off", "On"],
       value: "Off"
@@ -62,7 +83,7 @@ module AbrStateMethods::OH
     "Telephone Number": {
       method: "phone"
     },
-    "Email Address": {
+    "Email": {
       method: "email"
     },
   }
@@ -164,39 +185,39 @@ module AbrStateMethods::OH
         "Wyandot",
       ]}},
       {"has_mailing_address": {type: :checkbox}},
-      {"Street Address or PO  box": {visible: "has_mailing_address"}},
-      {"CityVillage_2": {visible: "has_mailing_address"}},
+      {"Street Address or PO Box": {visible: "has_mailing_address"}},
+      {"City_2": {visible: "has_mailing_address"}},
       {"State": {visible: "has_mailing_address", type: :select, options: GeoState.collection_for_select, include_blank: true}},
       {"ZIP_2": {visible: "has_mailing_address", min: 5, max: 10}},
       {"identification": {
         type: :radio, 
         required: true,
         options:["dln", "ssn4", "photoid"]}},
-      {"OR": {visible: "identification_dln", required: "show_star", min: 8, max: 8, ui_regexp: "^[a-zA-Z]{2}[0-9]{6}$"}},
-      {"OR_2": {visible: "identification_ssn4", required: "show_star",  min: 4, max: 4, regexp: /\A\d{4}\z/ }},      
+      {"Ohio_driver_license": {visible: "identification_dln", required: "show_star", min: 8, max: 8, ui_regexp: "^[a-zA-Z]{2}[0-9]{6}$"}},
+      {"SSN4": {visible: "identification_ssn4", required: "show_star",  min: 4, max: 4, regexp: /\A\d{4}\z/ }},      
       {"dln_soft_validation": {type: :hidden}},
     ]
   end
   
   def custom_form_field_validations
     if self.has_mailing_address.to_s == "1"
-      ["Street Address or PO  box", "CityVillage_2", "State", "ZIP_2"].each do |f|
+      ["Street Address or PO Box", "City_2", "State", "ZIP_2"].each do |f|
         custom_validates_presence_of(f)
         #errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
       end
     end
     if self.identification == "dln"
-      custom_validates_presence_of("OR")
+      custom_validates_presence_of("Ohio_driver_license")
       # f = "OR"
       # errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
     end
     if self.identification == "ssn4"
-      custom_validates_presence_of("OR_2")
+      custom_validates_presence_of("SSN4")
       # f = "OR_2"
       # errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
     end
   end
-  
+=begin
   def identification
     return "dln" if self.send(self.class.make_method_name("Your Ohio driver license number 2 letters followed by 6 numbers")) == "On"
     return "ssn4" if self.send(self.class.make_method_name("Last four digits of your Social Security number")) == "On"
@@ -215,6 +236,6 @@ module AbrStateMethods::OH
       self.send(self.class.make_method_name("Copy of a current and valid photo identification military identification or a current within the="), "On")
     end
   end
-  
+=end
  
 end
