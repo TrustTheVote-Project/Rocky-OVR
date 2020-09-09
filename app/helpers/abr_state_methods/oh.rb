464cd1f6f0aa7a99131b10ea0c926ba8145b4fe4
Christopher <cwisdo@gnaritas.com>
2020-09-09 12:20:14 -0400

    Removes old code

--- app/helpers/abr_state_methods/oh.rb
+++ app/helpers/abr_state_methods/oh.rb
@@ -13,9 +13,6 @@ module AbrStateMethods::OH
     "Suffix": {
       method: "name_suffix"
     },
-    # Old "Date of Birth do not write todays date here": {
-    #  method: "date_of_birth_mm_dd_yyyy"
-    #},
     "DOB_day": {
       method:"date_of_birth_mm"  # Note this is reversed in form
     },
@@ -25,46 +22,24 @@ module AbrStateMethods::OH
     "DOB_yr": {
       method: "date_of_birth_yyyy"
     },
-    #"Street Address no PO  boxes": {
     "Street Address": {
       method: "address"
     },
     "County": {},
-    # Old "CityVillage": {
-    #  method: "city"
-    #},
     "City": {},
     "ZIP": {
       method: "zip"
     },
-    #mailing address
-    #old "Street Address or PO  box": {},
     "Street Address or PO Box": {},
-    # old "CityVillage_2": {},
     "City_2": {},
     "State": {},
     "ZIP_2": {},
-    #"OR": {sensitive: true}, #drivers license number
     "Ohio_driver_license": {sensitive: true},
     
-=begin  
-     "Your Ohio driver license number 2 letters followed by 6 numbers": {
-      options: ["Off", "On"]
-    },
-    "Last four digits of your Social Security number": {
-   
-      options: ["Off", "On"]
-    },
-    "Copy of a current and valid photo identification military identification or a current within the": {
-      options: ["Off", "On"]
-    },
-=end
-    #"OR_2": {sensitive: true}, #last 4 of SS
     "SSN4": {sensitive: true},
     "Date of Election do not write todays date here": {
       value: "11/03/2020"
     },
-    #old "General Election": {
     "November General Election": {
       options: ["Off", "On"],
       value: "On"
@@ -203,39 +178,14 @@ module AbrStateMethods::OH
     if self.has_mailing_address.to_s == "1"
       ["Street Address or PO Box", "City_2", "State", "ZIP_2"].each do |f|
         custom_validates_presence_of(f)
-        #errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
       end
     end
     if self.identification == "dln"
       custom_validates_presence_of("Ohio_driver_license")
-      # f = "OR"
-      # errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
     end
     if self.identification == "ssn4"
       custom_validates_presence_of("SSN4")
-      # f = "OR_2"
-      # errors.add(self.class.make_method_name(f), custom_required_message(f)) if self.send(self.class.make_method_name(f)).blank?
     end
   end
-=begin
-  def identification
-    return "dln" if self.send(self.class.make_method_name("Your Ohio driver license number 2 letters followed by 6 numbers")) == "On"
-    return "ssn4" if self.send(self.class.make_method_name("Last four digits of your Social Security number")) == "On"
-    return "photoid" if self.send(self.class.make_method_name("Copy of a current and valid photo identification military identification or a current within the")) == "On"
-  end
-  
-  def identification=(val)
-    self.send(self.class.make_method_name("Your Ohio driver license number 2 letters followed by 6 numbers="), "Off")
-    self.send(self.class.make_method_name("Last four digits of your Social Security number="), "Off")
-    self.send(self.class.make_method_name("Copy of a current and valid photo identification military identification or a current within the="), "Off")
-    if val == "dln"
-      self.send(self.class.make_method_name("Your Ohio driver license number 2 letters followed by 6 numbers="), "On")
-    elsif val == "ssn4"
-      self.send(self.class.make_method_name("Last four digits of your Social Security number="), "On")
-    elsif val == "photoid"
-      self.send(self.class.make_method_name("Copy of a current and valid photo identification military identification or a current within the="), "On")
-    end
-  end
-=end
- 
+
 end
