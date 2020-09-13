7e06a6d693e4c42e97d9362f2de5eb0b94d14790
Christopher <cwisdo@gnaritas.com>
2020-09-13 14:53:26 -0400

    Implements PA PDF form filling

--- app/helpers/abr_state_methods/pa.rb
+++ app/helpers/abr_state_methods/pa.rb
@@ -41,10 +41,12 @@ module AbrStateMethods::PA
       "City_Town": {},
       "City_Town_1": {},
       "SSN_last_4": {},
-      "no_id": {},
+      "no_id": {
+          method: "check_assert_no_id"
+      },
       "Same_as_above": { options: ["Off", "On"] },
     }
-    EXTRA_FIELDS = ["no_PennDOT"]
+    EXTRA_FIELDS = ["no_PennDOT", "assert_no_id"]
     
     
     def form_field_items
@@ -119,16 +121,16 @@ module AbrStateMethods::PA
           "York",
         ]}},
         {"Municipality": {}},
-        {"Lived_at_Address_Since": {required: true}},
-        {"PA drivers license or PennDOT ID card number": {}},
+        {"Lived_at_Address_Since": {required: true, regexp: /\A[0-9]{2}\/[0-9]{2}\/[0-9]{4}\z/}},
+        {"PA drivers license or PennDOT ID card number": {regexp: /\A\d{8}\z/, length:8}},
         {"no_PennDOT": {type: :checkbox}},
-        {"SSN_last_4": {visible: "no_PennDOT"}},
-        {"no_id": {type: :checkbox, visible: "no_PennDOT"}},
-        {"Same_as_above": {type: :radio}},
-        {"Address_1": {visible: "same_as_above_off"}},
-        {"City_Town_1": {visible: "same_as_above_off", classes: "half"}},
-        {"State": {visible: "same_as_above_off", classes: "quarter", type: :select, options: GeoState.collection_for_select, include_blank: true}},
-        {"Zip code_2": {visible: "same_as_above_off", classes: "quarter last"}},
+        {"SSN_last_4": {visible: "no_PennDOT", classes:"half", length:4,regexp: /\A[0-9]{4}\z/}},
+        {"assert_no_id": {type: :checkbox, visible: "no_PennDOT"}},
+        {"Same_as_above": {type: :radio, required: true}},
+        {"Address_1": {visible: "same_as_above_off", required: 'star'}},
+        {"City_Town_1": {visible: "same_as_above_off", classes: "half", required:'star'}},
+        {"State": {visible: "same_as_above_off", required: 'star', classes: "quarter", type: :select, options: GeoState.collection_for_select, include_blank: true}},
+        {"Zip code_2": {visible: "same_as_above_off", required:'star', classes: "quarter last"}},
       ]
     end
     #e.g.
@@ -148,12 +150,41 @@ module AbrStateMethods::PA
     #   {"OR_2": {visible: "identification_ssn4", min: 4, max: 4, regexp: /\A\d{4}\z/}},
     # ]
     
+    def check_assert_no_id
+        return ("X") if self.assert_no_id.to_s=='1'
+    end
+
+
+    REQUIRED_MAILING_ADDRESS_FIELDS = [
+        "Address_1",
+        "City_Town_1",
+        "State",
+        "Zip code_2",
+      ]
+    
+      def require_mailing_fields
+        REQUIRED_MAILING_ADDRESS_FIELDS.each do |field|
+          custom_validates_presence_of(field)
+        end
+      end
+
     
     def custom_form_field_validations
       # e.g:
       # make sure delivery is selected if reason ==3
       # e.g:
       # make sure fax is provided if faxtype is selected for delivery
+      if (self.no_PennDOT.to_s!='1')
+        custom_validates_presence_of('PA drivers license or PennDOT ID card number')  
+      elsif (self.assert_no_id.to_s!='1')
+        custom_validates_presence_of('SSN_last_4')
+      end
+
+      if self.same_as_above.to_s=='Off'
+        require_mailing_fields
+      else
+          
+      end
     end
     
    
