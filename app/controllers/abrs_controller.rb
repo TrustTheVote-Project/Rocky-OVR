97a5fb299e6ad623390a113f92e6766b99f017ff
Alexander Mekelburg <alexmek@gmail.com>
2020-09-17 16:27:25 -0400

    fix address test, labels

--- app/controllers/abrs_controller.rb
+++ app/controllers/abrs_controller.rb
@@ -149,7 +149,7 @@ class AbrsController < ApplicationController
   
   private
   def abr_params
-    attrs = [:first_name, :middle_name, :last_name, :name_suffix, :email, :street_name, :street_number, :city, :zip, :registration_county, :date_of_birth_month, :date_of_birth_day, :date_of_birth_year, :votercheck, :phone, :phone_type, :opt_in_email, :opt_in_sms, :partner_opt_in_email, :partner_opt_in_sms, :tracking_id, :tracking_source]
+    attrs = [:first_name, :middle_name, :last_name, :name_suffix, :email, :street_name, :street_number, :unit, :city, :zip, :registration_county, :date_of_birth_month, :date_of_birth_day, :date_of_birth_year, :votercheck, :phone, :phone_type, :opt_in_email, :opt_in_sms, :partner_opt_in_email, :partner_opt_in_sms, :tracking_id, :tracking_source]
     if @abr
       attrs += @abr.permitted_attrs
       attrs += @abr.allowed_signature_attrs
