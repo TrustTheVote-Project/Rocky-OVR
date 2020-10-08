608c7b3b866cfa5a8216b5239c096c9a9eb3a404
Alexander Mekelburg <alexmek@gmail.com>
2020-10-07 21:28:39 -0400

    fix for missing phone type

--- db/schema.rb
+++ db/schema.rb
@@ -788,6 +788,15 @@ ActiveRecord::Schema.define(version: 20200929202144) do
     t.boolean  "partner_opt_in_email"
     t.boolean  "partner_volunteer"
     t.integer  "penndot_retries",                                  default: 0
+    t.boolean  "request_abr"
+    t.string   "abr_address_type"
+    t.string   "abr_ballot_address"
+    t.string   "abr_ballot_city"
+    t.string   "abr_ballot_state"
+    t.string   "abr_ballot_zip"
+    t.string   "abr_ballot_address_start_year"
+    t.string   "abr_ward"
+    t.boolean  "abr_declaration"
   end
 
   add_index "state_registrants_pa_registrants", ["original_partner_id"], name: "pa_registrants_original_partner_id"
