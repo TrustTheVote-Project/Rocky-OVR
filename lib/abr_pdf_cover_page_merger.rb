25c8f73dadf7d2923b238944f562d5d74dd2f537
Christopher <cwisdo@gnaritas.com>
2020-10-20 17:57:08 -0400

    Debugs pdff cover page merger utility

--- lib/abr_pdf_cover_page_merger.rb
+++ lib/abr_pdf_cover_page_merger.rb
@@ -1,37 +1,42 @@
 class AbrPdfCoverPageMerger
     
-    def pdf_template_path (home_state_abbrev, pdf_template)
+    def self.pdf_template_path (home_state_abbrev, pdf_template_name)
         abbrev = home_state_abbrev.downcase
         Rails.root.join("data/abr_pdfs/#{abbrev}/#{pdf_template_name}")
     end
 
-    def pdf_template_with_cover_filename(pdf_template_name)
+    def self.pdf_template_with_cover_filename(pdf_template_name)
         pdf_template_name.gsub(/\.pdf\z/, '_with_cover.pdf')
     end
 
-    def pdf_template_with_cover_path (home_state_abbrev, pdf_template)
+    def self.pdf_template_with_cover_path (home_state_abbrev, pdf_template_name)
         abbrev = home_state_abbrev.downcase
-        Rails.root.join("data/abr_pdfs/#{abbrev}/#{pdf_template_with_cover_filename}")
+        Rails.root.join("data/abr_pdfs/#{abbrev}/#{pdf_template_with_cover_filename(pdf_template_name)}")
     end
 
-    def pdf_cover_path 
-        Rails.root.join("data/abr_pdfs/abr_coverpage/coverpage.pdf")
+    def self.pdf_cover_path 
+        Rails.root.join("data/abr_coverpage/coverpage.pdf")
     end
 
-    def make_cover_version(state_abbrev, pdf_template)
-        `pdftk #{pdf_cover_path} #{pdf_template_path(home_state_abbrev, pdf_template)} output #{pdf_template_with_cover_path(home_state_abbrev, pdf_template)}`
+    def self.make_cover_version(state_abbrev, pdf_template_name)
+        cover_path=self.pdf_cover_path
+        template_path=self.pdf_template_path(state_abbrev, pdf_template_name)
+        template_with_cover_path=self.pdf_template_with_cover_path(state_abbrev, pdf_template_name)
+
+        `pdftk #{cover_path} #{template_path} output #{template_with_cover_path}`
     end
 
-    def build_coverpage_versions
+    def self.build_coverpage_versions
         RockyConf.absentee_states.each do |state_abbrev, state_config| 
             if state_config&.pdf_template
-                make_cover_version(state_abbrev,state_config&.pdf_template)
+                self.make_cover_version(state_abbrev,state_config&.pdf_template)
             end
         end
     end
 
     def self.run
-        build_coverpage_versions
+        self.build_coverpage_versions
+        return(true)
     end
 
 end
