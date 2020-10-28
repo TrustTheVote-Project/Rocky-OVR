f2d4d8f522bb78259bf4cec94e0b595afe988c25
Christopher <cwisdo@gnaritas.com>
2020-10-28 15:58:57 -0400

    delegates for custom state registrants

--- app/models/state_registrants/base.rb
+++ app/models/state_registrants/base.rb
@@ -9,6 +9,10 @@ class StateRegistrants::Base < ActiveRecord::Base
   delegate :titles, :suffixes, :races, :state_parties, :phone_types, :partner, :partner_id, :state_registrar_address, :rtv_and_partner_name, :home_state_email_instructions, :email_address_to_send_from,  :finish_iframe_url, :javascript_disabled?, :canvassing_shift, to: :registrant
   delegate :has_phone?, :is_fake?, :requires_race?, :requires_party?, :require_age_confirmation?, :require_id?, :en_localization, :to => :registrant
   
+  #delegate :ab_tests,:full_name,:finish_with_state_eligible?, to: :registrant
+  
+  delegate :finish_with_state_eligible?,:prev_name_title,:prev_first_name,:prev_middle_name,:prev_last_name,:prev_name_suffix,:prev_address,:prev_unit,:home_address, to: :registrant
+
   delegate :ask_for_partner_volunteers?, to: :registrant
 
   def check_valid_for_state_flow!
