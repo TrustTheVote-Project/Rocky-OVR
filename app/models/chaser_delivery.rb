4238d949237f28d949900981f9df85bb9f9aebaf
Alexander Mekelburg <alexmek@gmail.com>
2021-11-29 10:59:16 -0500

    fix spelling, update staging2 chaser delivery window

--- app/models/chaser_delivery.rb
+++ app/models/chaser_delivery.rb
@@ -3,7 +3,7 @@ class ChaserDelivery < ApplicationRecord
   belongs_to :abr, optional: true
   
   def self.can_send_chaser?(email)
-    prev_delivery = ChaserDelivery.where(email: email).where("created_at > ?", RockyConf.hours_before_chaser_redilivery.hours.ago)
+    prev_delivery = ChaserDelivery.where(email: email).where("created_at > ?", RockyConf.hours_before_chaser_redelivery.hours.ago)
     return prev_delivery.count == 0
   end
 end
