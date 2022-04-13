51625935c2291c69eca7afeffe3bb86d4a7dd020
Alexander Mekelburg <alexmek@gmail.com>
2022-04-13 16:34:27 -0400

    sd form

--- app/helpers/abr_state_methods/sd.rb
+++ app/helpers/abr_state_methods/sd.rb
@@ -11,21 +11,22 @@ module AbrStateMethods::SD
     "abr_address_city_state": { method: "address_city_state" },
     "abr_county": { },	
     "abr_zip": { method: "zip" },	
-    "abr_check_mailing_address": {  options: ["Off", "Yes"] },	
+    "abr_check_mailing_address": {  options: ["Off", "On"] },	
     "abr_mailing_address_line_1": { },
-    "abr_mailing_city_state": { },
+    "abr_mailing_city_state": { method: "get_abr_mailing_city_state" },
     "abr_mailing_zip": { },	
     "abr_election_type_selections": { options: [
       "abr_election_type1",
       "abr_election_type2",
     ]},
-    "abr_election_type3": { options: ["Off", "Yes"]},	
-    "abr_election_type4": { options: ["Off", "Yes"]},	
-    "abr_election_type5": { options: ["Off", "Yes"]},	
-    "abr_election_type6": { options: ["Off", "Yes"]},	
-    "abr_election_type7": { options: ["Off", "Yes"]},	
-    "abr_application_type_check1": { options: ["Off", "Yes"]},	
-    "abr_primary_type_selections1": { options: [
+    "abr_election_type1": { method: "abr_election_type_selections_is_abr_election_type1", options: ["Off", "On"]},
+    "abr_election_type3": { options: ["Off", "On"]},	
+    "abr_election_type4": { options: ["Off", "On"]},	
+    "abr_election_type5": { options: ["Off", "On"]},	
+    "abr_election_type6": { options: ["Off", "On"]},	
+    "abr_election_type7": { options: ["Off", "On"]},	
+    "abr_application_type_check1": { options: ["Off", "On"]},	
+    "abr_primary_type_selection": { options: [
       "abr_primary_type1",
       "abr_primary_type2",
       "abr_primary_type3",
@@ -42,18 +43,32 @@ module AbrStateMethods::SD
       "abr_id_type1",
       "abr_id_type2",
     ]},
-    "abr_request_check": { options: ["Off", "Yes"] },	
+    "abr_request_check": { options: ["Off", "On"] },	
     "abr_assistant_first_name": { },	
     "abr_assistant_last_name": { },	
     "abr_assistant_phone": { },	
     "abr_assistant_address_line_1": { },	
     "abr_assistant_line_2": { },	
-    "abr_assistant_address_city_state": { },
+    "abr_assistant_address_city_state": { method: "get_abr_assistant_address_city_state" },
     "abr_assistant_zip": { },	
     "abr_phone": { method: "phone" },	
   }
-  #EXTRA_FIELDS = ["has_mailing_address", "messenger", "uocava", "uocava_email"]
+  EXTRA_FIELDS = ["abr_mailing_state", "abr_mailing_city", "abr_assistant_address_city", "abr_assistant_address_state"]
   
+  def abr_election_type_selections_is_abr_election_type1
+    if abr_election_type_selections_abr_election_type1 
+      return "On"
+    end
+    return "Off"
+  end
+
+  def get_abr_mailing_city_state
+    [abr_mailing_state, abr_mailing_city].join(', ')
+  end
+
+  def get_abr_assistant_address_city_state
+    [abr_assistant_address_city, abr_assistant_address_state].join(', ')
+  end
   
   def form_field_items
     [
@@ -127,104 +142,38 @@ module AbrStateMethods::SD
       ]}},
       {"abr_check_mailing_address": { type: :checkbox }},
       {"abr_mailing_address_line_1": { visible: "abr_check_mailing_address", required: :if_visible }},
-      {"abr_mailing_city_state": { visible: "abr_check_mailing_address", required: :if_visible }},
-      {"abr_mailing_zip": { visible: "abr_check_mailing_address", required: :if_visible }},
+      {"abr_mailing_city": {classes: "half", visible: "abr_check_mailing_address", required: :if_visible }},
+      {"abr_mailing_state": {classes: "quarter", visible: "abr_check_mailing_address", required: :if_visible,
+        type: :select, options: GeoState.collection_for_select
+      }},
+      {"abr_mailing_zip": {classes: "quarter", visible: "abr_check_mailing_address", required: :if_visible }},
       {"abr_election_type_selections": { required: true, type: :radio }},
       {"abr_election_type3": { type: :checkbox, visible: "abr_election_type_selections_abr_election_type2" }},
       {"abr_election_type4": { type: :checkbox, visible: "abr_election_type_selections_abr_election_type2" }},
       {"abr_election_type5": { type: :checkbox, visible: "abr_election_type_selections_abr_election_type2" }},
       {"abr_election_type6": { type: :checkbox, visible: "abr_election_type_selections_abr_election_type2" }},
       {"abr_election_type7": { type: :checkbox, visible: "abr_election_type_selections_abr_election_type2" }},
-      {"abr_application_type_check1": { type: :checkbox, visible: "abr_election_type4" }},
-      {"abr_primary_type_selections1": { required: true, type: :radio, visible: "abr_election_type_selections_abr_election_type1"  }},
-      {"abr_residency_requirements_instructions": {type: :instructions, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type6 abr_election_type7"}},
-      {"abr_residency_requirements_selections1": { type: :radio, required: :if_visible, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type6 abr_election_type7"}},
-      {"abr_residency_requirements_selections2": { type: :radio, required: :if_visible, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type6 abr_election_type7" }},
+      {"abr_election_type_hint": { type: :instructions, visible_any: "abr_election_type4 abr_election_type_selections_abr_election_type1" }},
+      {"abr_application_type_check1": { type: :checkbox, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type4" }},
+      {"abr_primary_type_selection": { type: :radio, visible: "abr_application_type_check1"  }},
+      {"abr_residency_requirements_instructions": {type: :instructions, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type5 abr_election_type6"}},
+      {"abr_residency_requirements_selections1": { type: :radio, required: :if_visible, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type6 abr_election_type5"}},
+      {"abr_residency_requirements_selections2": { type: :radio, required: :if_visible, visible_any: "abr_election_type_selections_abr_election_type1 abr_election_type6 abr_election_type5" }},
       {"abr_id_selections": { type: :radio }},
       {"abr_request_check": { type: :checkbox }},
-      {"abr_assistant_first_name": { visible: "abr_request_check", required: :if_visible }},
-      {"abr_assistant_last_name": { visible: "abr_request_check", required: :if_visible  }},
+      {"abr_assistant_first_name": { classes: "half", visible: "abr_request_check", required: :if_visible }},
+      {"abr_assistant_last_name": { classes: "half", visible: "abr_request_check", required: :if_visible  }},
       {"abr_assistant_phone": { visible: "abr_request_check", required: :if_visible }},
-      {"abr_assistant_address_line_1": { visible: "abr_request_check", required: :if_visible }},
-      {"abr_assistant_line_2": { }},
-      {"abr_assistant_address_city_state": { visible: "abr_request_check", required: :if_visible }},
-      {"abr_assistant_zip": { visible: "abr_request_check", required: :if_visible }},
+      {"abr_assistant_address_line_1": {classes: "three-quarter",  visible: "abr_request_check", required: :if_visible }},
+      {"abr_assistant_line_2": { classes: "quarter", visible: "abr_request_check", }},
+      {"abr_assistant_address_city": { classes: "half", visible: "abr_request_check", required: :if_visible }},
+      {"abr_assistant_address_state": { classes: "quarter", visible: "abr_request_check", required: :if_visible,
+        type: :select, options: GeoState.collection_for_select
+      }},
+      {"abr_assistant_zip": { classes: "quarter", visible: "abr_request_check", required: :if_visible }},
     ]
   end
   
-  # "Box 14": { options: ["Off", "Yes"] }, # uocava_1 yes
-  # "Box 15": { options: ["Off", "Yes"] }, # uocava_1 no
-  def uocava_1
-    if self.box_14 == "Yes"
-      return "Yes"
-    elsif self.box_15 == "Yes"
-      return "No"
-    end
-    return nil
-  end
-  def uocava_1=(value)
-    if value == "Yes"
-      self.box_14 = "Yes"
-      self.box_15 = "Off"
-    elsif value == "No"
-      self.box_14 = "Off"
-      self.box_15 = "Yes"
-    end
-  end
-  
-  # "Box 16": { options: ["Off", "Yes"] }, # uocava_2 yes
-  # "Box 17": { options: ["Off", "Yes"] }, # uocava_2 no
-  def uocava_2
-    if self.box_16 == "Yes"
-      return "Yes"
-    elsif self.box_17 == "Yes"
-      return "No"
-    end
-    return nil
-    
-  end
-  def uocava_2=(value)
-    if value == "Yes"
-      self.box_16 = "Yes"
-      self.box_17 = "Off"
-    elsif value == "No"
-      self.box_16 = "Off"
-      self.box_17 = "Yes"
-    end
-  end
-  
-  
-  # "Box 18": { options: ["Off", "Yes"] }, # uocava_3 yes
-  # "Box 19": { options: ["Off", "Yes"] }, # uocava_3 no
-  def uocava_3
-    if self.box_18 == "Yes"
-      return "Yes"
-    elsif self.box_19 == "Yes"
-      return "No"
-    end
-    return nil
-    
-  end
-  def uocava_3=(value)
-    if value == "Yes"
-      self.box_18 = "Yes"
-      self.box_19 = "Off"
-    elsif value == "No"
-      self.box_18 = "Off"
-      self.box_19 = "Yes"
-    end
-    
-  end
-  
-  
-  
-  def email_if_military_opt_in
-    if uocava == "1" && uocava_email == "Yes"
-      email
-    else
-      nil
-    end
-  end
   
   
   def custom_form_field_validations
