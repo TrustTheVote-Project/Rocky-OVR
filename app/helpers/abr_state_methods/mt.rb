b5f15de16f48d2e2e95594b501796878eba3d9bd
Alexander Mekelburg <alexmek@gmail.com>
2022-05-23 10:38:54 -0400

    implement UI for MT

--- app/helpers/abr_state_methods/mt.rb
+++ app/helpers/abr_state_methods/mt.rb
@@ -1,49 +1,52 @@
 module AbrStateMethods::MT
   
-  PDF_FIELDS = {'Period mmddyyyymmddyyyy': {},
-                'Signature of Elector': {},
-                'Date Signed': {},
-                'Date ballot received': {},
-                'Signature of Designee': {},
-                'Signature of Elector_2': {},
-                'Date Signed_2': {},
-                'abr_last_name': {:method=>"last_name"},
+  PDF_FIELDS = {'abr_last_name': {:method=>"last_name"},
                 'abr_first_name': {:method=>"first_name"},
-                'abr_middle_name': {},
-                'phone': {},
-                'abr_county': {:method=>"county"},
+                'abr_middle_name': {method: "middle_name"},
+                'abr_phone': {method: "phone"},
+                'abr_street_number': {method: "street_number"},
+                'abr_street_name': {method: "street_name"},
                 'abr_city': {:method=>"city"},
-                'abr_mailing_zip': {:method=>"zip"},
-                'abr_mailing_street_number': {},
-                'abr_mailing_city': {:method=>"city"},
-                'abr_mailing_state_abbrev': {:method=>"home_state_abbrev"},
                 'abr_zip': {:method=>"zip"},
-                'abr_mailing_street_name': {},
-                'abr_mailing_unit': {:method=>"unit"},
-                'seasonal_zip': {},
-                'seasonal_Mailing_Address': {},
-                'seasonal_City_State': {},
-                'abr_election_type_selections': {},
-                'abr_election_type_selections2': {},
-                'abr_election_type6_name': {},
-                'abr_election_date': {},
-                'abr_request_name': {},
-                'send_pamphlet': {},
-                'dont_want_ab': {},
-                'abr.email': {},
-                'date_of_birth_mm_dd_yyyy': {},
-                'abr_street_number': {},
-                'abr_street_name': {},
                 'abr_unit': {:method=>"unit"},
-                'is_mailing_addr_seasonal': {}
+                
+                'abr_county': {},
+                'abr_mailing_street_number': {},
+                'abr_mailing_street_name': {},
+                'abr_mailing_city': {},
+                'abr_mailing_state_abbrev': {},
+                'abr_mailing_unit': {},
+                'abr_mailing_zip': {},
+                'abr_election_type_selections': {
+                  options: [
+                    "abr_election_type1",
+                    "abr_election_type2",
+                  ]
+                },
+                'abr_election_type_selections2': {
+                  options: [
+                    "abr_election_type3",
+                    "abr_election_type4",
+                    "abr_election_type5",
+                    "abr_election_type6",
+                  ]
+                },
+                'abr_election_type6_name': {},
+                'abr_election_date': {method: "abr_election_date_string"},
+                'abr_request_name': {},
+                'abr.email': { method: "email"},
+                'abr_date_of_birth_mm_dd_yyyy': {
+                  method: "date_of_birth_mm_dd_yyyy",
+                  pdf_name: "date_of_birth_mm_dd_yyyy"
+                },
                }
 
-  EXTRA_FIELDS = ["has_mailing_address", "designee",  "period_from_mm", "period_from_dd", "period_from_yyyy", "period_to_mm", "period_to_dd", "period_to_yyyy"]
+  EXTRA_FIELDS = ["abr_check_mailing_address", "abr_request_check", "abr_election_date_input_mm", "abr_election_date_input_dd", "abr_election_date_input_yyyy"]
   
   
   def form_field_items
     [
-      {"County where you reside and are registered to vote": {type: :select, required: true, include_blank: true, options: [
+      {"abr_county": {type: :select, required: true, include_blank: true, options: [
         "Beaverhead",
         "Big Horn",
         "Blaine",
@@ -102,63 +105,30 @@ module AbrStateMethods::MT
         "Yellowstone",
         "Yellowstone National Park",
       ]}},
-      {"address_instructions": {type: :instructions}},
-      {"has_mailing_address": {type: :checkbox}},
-      {"Mailing Address required if differs from residence address": {visible: "has_mailing_address", required: :if_visible}},
-      {"City and State": {visible: "has_mailing_address", required: :if_visible}},
-      {"Zip Code_2": {visible: "has_mailing_address", required: :if_visible}},
-      {"Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only": {type: :checkbox}},
-      {"Seasonal Mailing Address Optional": {visible: self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only"), required: :if_visible}},
-      {"City and State_2": {visible: self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only"), required: :if_visible}},
-      {"Zip Code_3": {visible: self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only"), required: :if_visible}},
-      {"period_from": {
-        visible: self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only"), 
-        type: :date, m: "period_from_mm", d: "period_from_dd", y: "period_from_yyyy"}},
-      {"period_to": {
-          visible: self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only"), 
-          type: :date, m: "period_to_mm", d: "period_to_dd", y: "period_to_yyyy"}},
-      {"designee_instructions": {type: :instructions}},
-      {"designee": {type: :checkbox}},
-      {"Receipt of absentee ballot by designee I received the absentee ballot for the applicant on": {visible: "designee"}},
+      {'abr_check_mailing_address': {type: :checkbox}},
+      {'abr_mailing_street_number': {required: :if_visible, visible: "abr_check_mailing_address", classes: "quarter"}},
+      {'abr_mailing_street_name': {required: :if_visible, visible: "abr_check_mailing_address", classes: "half"}},
+      {'abr_mailing_unit': {visible: "abr_check_mailing_address", classes: "quarter"}},
+      {'abr_mailing_city': {required: :if_visible, visible: "abr_check_mailing_address", classes: "half"}},
+      {'abr_mailing_state_abbrev': {type: :select, required: :if_visible, visible: "abr_check_mailing_address", classes: "quarter", options: GeoState.collection_for_select}},
+      {'abr_mailing_zip': {required: :if_visible, visible: "abr_check_mailing_address", classes: "quarter"}},
+      {'abr_election_type_selections': {type: :radio, required: true}},
+      {'abr_election_type_selections2': {type: :radio, required: :if_visible, visible: "abr_election_type_selections_abr_election_type2"}},
+      {'abr_election_type6_name': {required: :if_visible, visible: "abr_election_type_selections2_abr_election_type6"}},
+      {'abr_election_date_input':  {type: :date, required: :if_visible, visible: "abr_election_type_selections2_abr_election_type6"}},
+      {'abr_request_check': {type: :checkbox}},
+      {'abr_request_name': {required: :if_visible, visible: "abr_request_check"}},
     ]
-  end
-  
-  def period_from
-    fdates = [period_from_mm, period_from_dd, period_from_yyyy].collect {|d| d.blank? ? nil : d}.compact
-    fdate  = fdates && fdates.length == 3 ? fdates.join("/") : nil
-  end
-  def period_to
-    tdates = [period_to_mm, period_to_dd, period_to_yyyy].collect {|d| d.blank? ? nil : d}.compact
-    tdate  = tdates && tdates.length == 3 ? tdates.join("/") : nil
-  end
-  
-  def period_from_to
-    [period_from, period_to].compact.join("-")
   end
   
   
   def custom_form_field_validations
-    if self.has_mailing_address == "1"
-      ["Mailing Address required if differs from residence address",
-      "City and State",
-      "Zip Code_2"].each do |f|
-        custom_validates_presence_of(f)
-      end
-    end
-    if self.send(self.class.make_method_name("Check if the mailing address listed above is for part of the year only and if so complete the information below for absentee ballot list only")) == "1"
-      [
-        "Seasonal Mailing Address Optional",
-        "City and State_2",
-        "Zip Code_3",
-        "period_from", "period_to"
-      ].each do |f|
-        custom_validates_presence_of(f)
-      end
-    end
-    if self.designee == "1"
-      custom_validates_presence_of(self.class.make_method_name("Receipt of absentee ballot by designee I received the absentee ballot for the applicant on"))
-    end
   end
+
+  def abr_election_date_string
+    date_field_string_mm_dd_yyyy(method: :abr_election_date_input)
+  end
+
   
  
 end
