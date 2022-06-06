241dfb685cb90f14112b7bf28d074c6385a67da4
Rock the Vote <david@rockthevote.com>
2022-06-06 14:07:44 -0700

    turn off nv iframe

--- app/models/state_customizations/nv.rb
+++ app/models/state_customizations/nv.rb
@@ -1,13 +1,13 @@
-class NV < StateCustomization
-  def online_reg_url(registrant)
-    root_url ="https://www.RegisterToVoteNV.gov/?source=rtv&utm_source=rtv&utm_medium=rtv&utm_campaign=rtv"
-    return root_url if registrant.nil?
-    lang = registrant.locale.to_s
-    "#{root_url}&lang=#{lang}"
-  end
+#class NV < StateCustomization
+ # def online_reg_url(registrant)
+  #  root_url ="https://www.RegisterToVoteNV.gov/?source=rtv&utm_source=rtv&utm_medium=rtv&utm_campaign=rtv"
+   # return root_url if registrant.nil?
+    #lang = registrant.locale.to_s
+    #"#{root_url}&lang=#{lang}"
+  #end
 
-  def redirect_to_online_reg_url(registrant)
-    return false
-  end
+  #def redirect_to_online_reg_url(registrant)
+  #  return false
+  #end
   
-end
+#end
