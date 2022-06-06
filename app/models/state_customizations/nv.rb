e40db6a0c54fd7e6c7affaaa9877aabe7c6c0046
Rock the Vote <david@rockthevote.com>
2022-06-06 13:50:08 -0700

    update NV url to https

--- app/models/state_customizations/nv.rb
+++ app/models/state_customizations/nv.rb
@@ -1,6 +1,6 @@
 class NV < StateCustomization
   def online_reg_url(registrant)
-    root_url ="http://www.RegisterToVoteNV.gov/?source=rtv&utm_source=rtv&utm_medium=rtv&utm_campaign=rtv"
+    root_url ="https://www.RegisterToVoteNV.gov/?source=rtv&utm_source=rtv&utm_medium=rtv&utm_campaign=rtv"
     return root_url if registrant.nil?
     lang = registrant.locale.to_s
     "#{root_url}&lang=#{lang}"
