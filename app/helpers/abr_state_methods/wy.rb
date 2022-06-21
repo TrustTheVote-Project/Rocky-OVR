9ab4b86db5140a65847bde67573042224d8b7ba7
Christopher <cwisdo@gnaritas.com>
2022-06-21 16:08:08 -0400

    2022 WY ABR

--- app/helpers/abr_state_methods/wy.rb
+++ app/helpers/abr_state_methods/wy.rb
@@ -1,83 +1,99 @@
 module AbrStateMethods::WY
   
   PDF_FIELDS = {
-      "LAST NAME": {
-        method: "last_name"
-      },
-      "FIRST NAME": {
-        method: "first_name"
-      },
-      "MIDDLE": {
-        method: "middle_name"
-      },      
-      "COUNTY RESIDENTIAL ADDRESS": {
-        method: "address"
-      },
-      "CITY": {
-        method: "city"
-      },
-      "STATE": {
-        method: "home_state_abbrev"
-      },
-      "ZIP": {
-        method: "zip"
-      },
-      "DATE OF BIRTH": {
-        method: "date_of_birth_mm_dd_yyyy"
-      },
-      "CONTACT PHONE NUMBER": {
-        method: "phone"
-      },
-      "ACTIVE MILITARY Y  N": {
-      },
-      "EMAIL optional": {
-        method: "email"
-      },
-      "MAIL MY BALLOT TO": {},
-      "CITY_2": {},
-      "STATE_2": {},
-      "ZIP_2": {},
-      "2020 PRIMARY": {	
-        options: ["Off", "On"],	
-        value: "Off"	
-      },	
-      "2020 GENERAL": {	
-        options: ["Off", "On"],	
-        value: "On"	
-      },	
-      "Individual's name who may pick up my ballot": {},	
-      "Date": {
-        method: "date_for_signature"
-      }
-      #voter_signature	      #voter_signature      
-  
+
+    "abr_email": {method: "email"},	
+    "abr_phone": {method: "phone"},	
+    "abr_first_name": {method: "first_name"},	
+    "abr_middle_name": {method: "middle_name"},	
+    "abr_last_name": {method: "last_name"},	
+    "abr_name_suffix": {method: "name_suffix"},	
+    "abr_street_number": {method: "street_number"},	
+    "abr_street_name": {method: "street_name"},	
+    "abr_unit": {method: "unit"},	
+    "abr_city": {method: "city"},	
+    "abr_home_state_abbrev": {method: "home_state_abbrev"},	
+    "abr_zip": {method: "zip"},	
+    'date_of_birth_mm_dd_yyyy':{ method: "date_of_birth_mm_dd_yyyy" },
+    "abr_phone":{mehtod:"phone"},
+
+# "abr_email":{},
+# "abr_first_name":{},
+# "abr_middle_name":{},
+# "abr_last_name":{},
+# "abr_street_number":{},
+# "abr_street_name":{},
+# "abr_unit":{},
+# "abr_city":{},
+# "abr_home_state_abbrev":{},
+# "abr_zip":{},
+# "date_of_birth_mm_dd_yyyy":{},
+# "abr_phone":{},
+
+"abr_check_mailing_address":{},
+"abr_mailing_address_line_1":{},
+"abr_mailing_unit":{},
+"abr_mailing_city":{},
+"abr_mailing_state_abbrev":{},
+"abr_mailing_zip":{},
+
+"abr_uocava_check":{ method: "abr_uocava_check_y_n"},
+
+"abr_election_type1":{ method: "abr_election_type1_check_on"},
+"abr_election_type2":{ method: "abr_election_type2_check_on"},
+"abr_party":{},
+"abr_request_check":{},
+"abr_request_name":{},
+
   }
 
-  def signature_pdf_field_name
-    "voter_signature"
-  end
 
-  EXTRA_FIELDS = ["has_mailing_address"]
+  # def signature_pdf_field_name
+  #   "voter_signature"
+  # end
+
+  EXTRA_FIELDS = ["abr_election_type1_check", "abr_election_type2_check"]
   
   def form_field_items
     [
-      {"has_mailing_address": {type: :checkbox}},
-      {"MAIL MY BALLOT TO": {visible: "has_mailing_address"}},
-      {"CITY_2": {visible: "has_mailing_address"}},
-      {"STATE_2": {visible: "has_mailing_address", type: :select, options: GeoState.collection_for_select}},
-      {"ZIP_2": {visible: "has_mailing_address", min: 5, max: 10}},
-      {"ACTIVE MILITARY Y  N": {required: true, type: :radio, options: ["Y", "N"]}},
-      {"Individual's name who may pick up my ballot": {}}
+
+      {"abr_check_mailing_address": {type: :checkbox}},	
+      {"abr_mailing_address_line_1": {classes: 'three-quarter', required: :if_visible, visible: "abr_check_mailing_address"}},	
+      {"abr_mailing_unit": {classes: 'quarter', required: false, visible: "abr_check_mailing_address"}},	
+      {"abr_mailing_city": {classes: 'half', required: :if_visible, visible: "abr_check_mailing_address"}},	
+      {"abr_mailing_state_abbrev": {type: :select, options: GeoState.collection_for_select, classes: 'quarter', required: :if_visible, visible: "abr_check_mailing_address"}},	
+      {"abr_mailing_zip": {classes: 'quarter', required: :if_visible, visible: "abr_check_mailing_address"}},	
+
+
+      {"abr_election_type_selections": {type: :instructions }},
+      {"abr_election_type1_check": { type: :checkbox}},
+      {"abr_election_type2_check": { type: :checkbox}},
+
+      {"abr_party": {visible: "abr_election_type1_check", required: :if_visible}},
+
+      {"abr_uocava_check": {required: true, type: :radio, options: ["Yes", "No"]}},
+      {"abr_request_check": {type: :checkbox}},
+      {"abr_request_name": {visible: "abr_request_check", required: :if_visible}}
     ]
   end
-  
+
   def custom_form_field_validations
-    if self.has_mailing_address.to_s == "1"
-      [:mail_my_ballot_to, :city_2, :state_2, :zip_2].each do |f|
-        errors.add(f, custom_required_message(f)) if self.send(f).blank?
-      end
+    if  self.abr_election_type1_check == "0" && self.abr_election_type2_check == "0" 
+      #errors.add("abr_election_type1", custom_required_message("no_election_type") )
+      errors.add("abr_election_type2_check", custom_required_message("no_election_type") )
     end
+
   end
+
+  def abr_election_type1_check_on
+    return "On" if abr_election_type1_check=="1"
+  end
+  def abr_election_type2_check_on
+    return "On" if abr_election_type2_check=="1"
+  end
+
+  
+
   
   
 end
