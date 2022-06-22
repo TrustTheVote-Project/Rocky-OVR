382da6e940b1575385524caaa87fa030588a4908
Christopher <cwisdo@gnaritas.com>
2022-06-22 12:11:13 -0400

    NH 2022 abr draft

--- app/helpers/abr_state_methods/nh.rb
+++ app/helpers/abr_state_methods/nh.rb
@@ -14,66 +14,42 @@ PDF_FIELDS = {
   "abr_home_state_abbrev": {method: "home_state_abbrev"},	
   "abr_zip": {method: "zip"},	
 
+  "abr_email": {method: "email_1"},
+  "abr_email2": {method: "email_2"},
+
+  "abr_county":{},
+  "abr_check_mailing_address":{},
+  "abr_mailing_address_line_1":{},
+  "abr_mailing_unit":{},
+  "abr_mailing_city":{},
+  "abr_mailing_state_abbrev":{},
+  "abr_mailing_zip":{},
+
+  "abr_form_instructions":{},
+  "abr_application_type_selections":{options:['abr_application_type1','abr_application_type2']},
+
+  "abr_reason_selections":{options:['abr_reason1','abr_reason2','abr_reason3','abr_reason4','abr_reason5','abr_reason6']},
+
+  "abr_reason_6_selections":{ options:['abr_reason6_type1','abr_reason6_type2'] },
+
+  "abr_election_type_selections":{options: ['abr_election_type1','abr_election_type2','abr_election_type3','abr_election_type4']},
+
+  "abr_party_selections":{options:['abr_party1','abr_party2']},
+
+  "abr_election_date":{method: "election_date_string"},
+  "abr_party_selections2":{options:['abr_party3','abr_party4']},
+
+  "abr_election_date2":{method: "election_date2_string"},
+  "abr_assistant_check1":{},
+  "abr_assistant_name":{},
+  "abr_phone":{ method: "phone_area"},
+  "abr_phone2":{method: "phone_prefix"},
+  "abr_phone3":{method: "phone_suffix"},}
 
 
 
-#"abr_email":{},
-"abr_email2":{method: "abr_email"},
-#"abr_first_name":{},
-#"abr_middle_name":{},
-#"abr_last_name":{},
-#"abr_name_suffix":{},
-#"abr_street_number":{},
-#"abr_street_name":{},
-#"abr_unit":{},
-#"abr_city":{},
-#"abr_home_state_abbrev":{},
-#"abr_zip":{},
-"abr_county":{},
-"abr_check_mailing_address":{},
-"abr_mailing_address_line_1":{},
-"abr_mailing_unit":{},
-"abr_mailing_city":{},
-"abr_mailing_state_abbrev":{},
-"abr_mailing_zip":{},
-
-"abr_form_instructions":{},
-"abr_application_type_selections":{},
-#"abr_application_type_selections":{},
-#"abr_application_type_selections":{},
-"abr_reason_selections":{options:['abr_reason1','abr_reason2','abr_reason3','abr_reason4','abr_reason5','abr_reason6']},
-# "abr_reason_selections":{},
-# "abr_reason_selections":{},
-# "abr_reason_selections":{},
-# "abr_reason_selections":{},
-# "abr_reason_selections":{},
-#"abr_reason6":{},
-"abr_reason_6_selections":{ options:['abr_reason6_type1','abr_reason6_type2'] },
-# "abr_application_type_selections":{},
-# "abr_application_type_selections":{},
-"abr_election_type_selections":{options: ['abr_election_type1','abr_election_type2','abr_election_type3','abr_election_type4']},
-# "abr_election_type_selections":{},
-# "abr_election_type_selections":{},
-# "abr_election_type_selections":{},
-# "abr_election_type_selections":{},
-"abr_party_selections":{options:['abr_party1','abr_party2']},
-# "abr_party_selections":{},
-# "abr_party_selections":{},
-"abr_election_date":{},
-"abr_party_selections2":{options:['abr_party3','abr_party4']},
-# "abr_party_selections2":{},
-# "abr_party_selections2":{},
-"abr_election_date2":{},
-"abr_assistant_check1":{},
-"abr_assistant_name":{},
-"abr_phone":{ method: "phone_area"},
-"abr_phone2":{method: "phone_prefix"},
-"abr_phone3":{method: "phone_suffix"},}
-
-
-
-EXTRA_FIELDS = ["abr_election_date_dd", "abr_election_date_mm", "abr_election_date_yyyy" ,
-  "abr_election_date2_dd", "abr_election_date2_mm", "abr_election_date2_yyyy"   ]
+EXTRA_FIELDS = ["abr_election_date_input_dd", "abr_election_date_input_mm", "abr_election_date_input_yyyy" ,
+  "abr_election_date2_input_dd", "abr_election_date2_input_mm", "abr_election_date2_input_yyyy"   ]
 
 #EXTRA_FIELDS = ["has_mailing_address", "assistant", "absentee_reason","storm_warning", "storm_warning_reason","mail_street_name", "mail_state", "mail_zip"]
  
@@ -93,8 +69,8 @@ EXTRA_FIELDS = ["abr_election_date_dd", "abr_election_date_mm", "abr_election_da
       {"abr_party_selections":{type: :radio, options:['abr_party1','abr_party2'], visible: "abr_election_type_selections_abr_election_type1",  required: :if_visible}},
       {"abr_party_selections2":{type: :radio, options:['abr_party3','abr_party4'], visible: "abr_election_type_selections_abr_election_type3",  required: :if_visible}},
 
-      {"abr_election_date":{type: :date, visible: "abr_election_type_selections_abr_election_type3",  required: :if_visible}},
-      {"abr_election_date2":{type: :date, visible: "abr_election_type_selections_abr_election_type4",  required: :if_visible}},
+      {"abr_election_date_input":{type: :date, visible: "abr_election_type_selections_abr_election_type3",  required: :if_visible}},
+      {"abr_election_date2_input":{type: :date, visible: "abr_election_type_selections_abr_election_type4",  required: :if_visible}},
 
 
       {"abr_county":{}},
@@ -124,19 +100,43 @@ EXTRA_FIELDS = ["abr_election_date_dd", "abr_election_date_mm", "abr_election_da
 
   def phone_area
     return if phone.blank?
-    return phone.slice(0,2)
+    return phone.slice(0,3)
   end
 
   def phone_prefix
     return if phone.blank?
-    return phone.slice(4,6)
+    return phone.slice(4,3)
   end
 
   def phone_suffix
     return if phone.blank?
-    return phone.slice(8,11)
+    return phone.slice(8,4)
   end
 
+  def email_1
+    return email.to_s.split("@").first
+  end
+
+  def email_2
+    return email.to_s.split("@").last;
+  end
+
+  def date_field_string_mm_dd_yy(field_opts)
+    d = date_field_value(field_opts)
+    if d
+      return d&.strftime("%m/%d/%y")
+    end
+  rescue
+    nil
+  end
+
+  def election_date_string
+    date_field_string_mm_dd_yy(method: :abr_election_date_input)
+  end
+
+  def election_date2_string
+    date_field_string_mm_dd_yy(method: :abr_election_date2_input)
+  end
 
  
 
