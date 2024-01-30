1bb3ecbc722d93d6f43e0cb1a5eae70cb450ef87
Rock the Vote <david@rockthevote.com>
2024-01-30 10:16:28 -0800

    tone back

--- app/validators/registrant_validator.rb
+++ app/validators/registrant_validator.rb
@@ -5,7 +5,7 @@ class RegistrantValidator < ActiveModel::Validator
     #regexp = /\A(none|\d{4}|([-*A-Z0-9]{7,42}(\s+\d{4})?))\z/i
     
     
-    reg.validates_format_of :phone, with: /\A(?!(\d)\1{9})\(*[1-9]\d{2}\)*[-\s]*[1-9]\d{2}[-\s]*\d{4}\z/, allow_blank: true
+    reg.validates_format_of :phone, with: /\A(?!([0-9])\1{9})[1-9]\d{2}[-\s]*\d{3}[-\s]*\d{4}\z/, allow_blank: true
     
     reg.validates_format_of :email_address, :with => Registrant::EMAIL_REGEX, :allow_blank => true
     validate_email(reg)
