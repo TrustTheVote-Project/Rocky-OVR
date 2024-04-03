e5e7cace59de55d0347d3fbcb346ac57c877c63c
Rock the Vote <david@rockthevote.com>
2024-04-02 17:12:48 -0700

    turn off zip3 validation

--- app/models/geo_state.rb
+++ app/models/geo_state.rb
@@ -416,7 +416,8 @@ class GeoState < ActiveRecord::Base
   end
 
   def self.for_zip_code(zip)
-    self[ zip5map[zip] || zip3map[zip] ]
+    self[zip5map[zip]]
+    #self[ zip5map[zip] || zip3map[zip] ]
   end
 
   def self.valid_zip_code?(zip)
