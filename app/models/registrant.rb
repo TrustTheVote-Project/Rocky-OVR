da7b803afaa4d9fbf87933d3669d3ad19dd5a6c3
Rock the Vote <david@rockthevote.com>
2024-04-06 05:45:53 -0700

    define iframe_param_present in registrant model

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -52,6 +52,11 @@ class Registrant < ActiveRecord::Base
     tracking_events.where(tracking_event_name: "registrant::render_view")
   end
 
+  # used to determine if mobile ui should be used in cases where iframed
+  def iframe_param_present?
+    other_parameters&.include?('iframe=true')
+  end
+
   
   
   serialize :state_ovr_data, Hash
