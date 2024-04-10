416903ae1c02cf0d4b149d44cc96767f25913901
Rock the Vote <david@rockthevote.com>
2024-04-10 11:32:24 -0700

    NE ABR updates

--- app/helpers/abr_state_methods/ne.rb
+++ app/helpers/abr_state_methods/ne.rb
@@ -8,13 +8,30 @@ module AbrStateMethods::NE
     "abr_name_suffix": {method: "name_suffix"},	
     "date_of_birth_mm_dd_yyyy": {method: "date_of_birth_mm_dd_yyyy"},
 
-
     "abr_street_name": {method: "street_name"},
     "abr_street_number": {method: "street_number"},
     "abr_unit": {method: "unit"},
     "abr_city": {method: "city"},
     "abr_home_state_abbrev": {method: "home_state_abbrev"},
     "abr_zip": {method: "zip"},
+
+	"abr_id_selections":{
+      options:[
+        'abr_id_type1',
+        'abr_id_type2',
+        'abr_id_type3',
+      ]
+    },
+    "abr_drivers_license": {sensitive:true},
+
+    "abr_primary_type_selections": {
+      options: [
+        "abr_primary_type1",
+        "abr_primary_type2",
+        "abr_primary_type3",
+        "abr_primary_type4",
+      ]
+    },
 
     "abr_address_type1": {
       method: "abr_address_type1_value",
@@ -26,32 +43,10 @@ module AbrStateMethods::NE
     "abr_mailing_city":{},   
     "abr_mailing_state_abbrev": {},
     "abr_mailing_zip": {},
-    "abr_election_type_selections": {
-      options: [
-        "abr_election_type1",
-        "abr_election_type2"
-      ]
-    },
-    "abr_election_date": {method: "election_date_string"},	
-    "abr_application_type_check1": {},	
-    "abr_application_type1_instructions": {},	
-    "abr_primary_type_selections1": {
-      options: [
-        "abr_primary_type1",
-        "abr_primary_type2",
-        "abr_primary_type3",
-        "abr_primary_type4",
-      ]
-    },
-    "abr_request_check": { options: ["Off", "Yes"]},	
-    "abr_request_name": {},
-    "abr_relationship1": {},	
+
     "abr_phone": { method: "phone"},	
   }
-  EXTRA_FIELDS = ["abr_mailing_address_city", "abr_mailing_address_state", "abr_mailing_address_zip",
-    "election_date_mm", "election_date_dd", "election_date_yyyy",
-    "abr_check_mailing_address",
-  ]
+  EXTRA_FIELDS = ["abr_mailing_address_city", "abr_mailing_address_state", "abr_mailing_address_zip", {name:"abr_drivers_license", sensitive:true}]
   
   def mailing_address_city_state_zip
     ["abr_mailing_address_city", "abr_mailing_address_state", "abr_mailing_address_zip"].collect {|f| 
@@ -69,21 +64,15 @@ module AbrStateMethods::NE
 
   def form_field_items
     [
+      {"abr_id_selections": {type: :radio, required: true, options: ["abr_id_type1", "abr_id_type2", "abr_id_type3"]}},
+      {"abr_drivers_license": {visible: "abr_id_selections_abr_id_type1", required: :if_visible, ui_regexp:"^[a-zA-Z][0-9]{8}$|^[0-9]{9}$", min:9, max:9}},
+      {"abr_primary_type_selections": {required: false, type: :radio}},
       {"abr_check_mailing_address": {type: :checkbox}},
       {"abr_mailing_address_line_1": {classes: "three-quarter", required: :if_visible, visible: "abr_check_mailing_address"}},
       {"abr_mailing_unit": {classes: "quarter", visible: "abr_check_mailing_address"}},
       {"abr_mailing_city": {classes: "half",  required: :if_visible, visible: "abr_check_mailing_address"}},
       {"abr_mailing_state_abbrev": {classes: "quarter",  required: :if_visible, visible: "abr_check_mailing_address", type: :select, options: GeoState.collection_for_select}},
       {"abr_mailing_zip": {classes: "quarter",  required: :if_visible, visible: "abr_check_mailing_address"}},
-      {"abr_election_type_selections": {required: true, type: :radio}},
-      {"election_date": {type: :date, required: :if_visible, visible: "abr_election_type_selections_abr_election_type2"}},
-      # {"abr_application_type_check1": {type: :checkbox}},
-      # {"abr_application_type1_instructions": {type: :instructions, visible: "abr_application_type_check1" }},
-      # {"abr_application_type1_instructions2": {type: :instructions, visible: "abr_application_type_check1"}},
-      # {"abr_primary_type_selections1": {type: :radio, visible: "abr_application_type_check1", required: :if_visible}},
-      {"abr_request_check": {type: :checkbox}},
-      {"abr_request_name": {required: :if_visible, visible: "abr_request_check"}},
-      {"abr_relationship1": {required: :if_visible, visible: "abr_request_check"}},
     ]
   end  
   
