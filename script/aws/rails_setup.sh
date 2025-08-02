7c1b173fa89befe32c3aeed67be200f0e431359b
Alexander Mekelburg <alexmek@gmail.com>
2025-08-02 00:16:59 -0400

    update setup script

--- script/aws/rails_setup.sh
+++ script/aws/rails_setup.sh
@@ -34,7 +34,7 @@ fi
 
 if [ $SERVER_ROLE == 'util' ]; then
     echo "I'm a util server"
-    
+    sudo dnf install -y cronie
     # Crontab is for UTIL only
     cd ~
     aws s3 cp s3://rocky-cloudformation-assets/crontab . --region us-west-2
@@ -107,16 +107,16 @@ if [ $SERVER_ROLE == 'web' ]; then
     touch tmp/restart.txt
     
     # Passenger monitoring Crontab is for WEB only
-    cd ~
-    aws s3 cp s3://rocky-cloudformation-assets/web-crontab . --region us-west-2
-    sed -i 's/RAILS_ENV/'"$RAILS_ENV"'/' ./web-crontab
+    # cd ~
+    # aws s3 cp s3://rocky-cloudformation-assets/web-crontab . --region us-west-2
+    # sed -i 's/RAILS_ENV/'"$RAILS_ENV"'/' ./web-crontab
     
     # Make sure the cron scripts are executable
-    chmod u+x /var/www/rocky/script/cron_cleanup_processes
+    # chmod u+x /var/www/rocky/script/cron_cleanup_processes
     
-    crontab -r
+    # crontab -r
     # Cat the crontab contents into the crontab editor
-    (crontab -l 2>/dev/null; cat ./web-crontab) | crontab -
+    # (crontab -l 2>/dev/null; cat ./web-crontab) | crontab -
     
 fi
 
