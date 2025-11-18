3f16f61b3016128166404d2cb3caa0d1b1a5b9e8
Rock the Vote <david@rockthevote.com>
2025-11-18 13:55:56 -0800

    Reject non-HTML requests to registration pages with HTTP 406
    Add format validation to prevent MissingTemplate errors on registration pages
    
    Bots requesting text/plain format were causing ActionView::MissingTemplate
    errors. Now return HTTP 406 (Not Acceptable) for non-HTML/JS requests.

--- app/controllers/registration_step.rb
+++ app/controllers/registration_step.rb
@@ -30,6 +30,7 @@ class RegistrationStep < ApplicationController
   layout "registration"
   before_action :find_partner
   before_action :find_canvassing_shift
+  before_action :force_html_format
 
   rescue_from Registrant::AbandonedRecord do |exception|
     reg = exception.registrant
@@ -69,6 +70,15 @@ class RegistrationStep < ApplicationController
   end
 
   protected
+
+  def force_html_format
+    # Only reject explicitly non-HTML formats (like text/plain, application/json, etc.)
+    # Allow */* (which browsers and monitoring tools use) to default to HTML
+    if request.format.symbol.present? && ![:html, :js, :all].include?(request.format.symbol)
+      head :not_acceptable
+    end
+  end
+
   def registrant_params
     #raise Registrant.column_names.to_s]
     if params[:registrant]
